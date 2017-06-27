7bd78a97c6cd4c85d69c493ed6cbbc63bb71d9d2
Spencer Chan <schan1031@gmail.com>
2017-06-27 16:38:02 -0700

    working session, some routes

--- W5D2/skeleton/lib/router.rb
+++ W5D2/skeleton/lib/router.rb
@@ -2,15 +2,25 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
+
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (@pattern =~ req.path) && (req.request_method.downcase.to_sym == @http_method)
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+    @controller_class.new(req, res, route_params).invoke_action(@action_name)
   end
 end
 
@@ -18,10 +28,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -32,13 +44,24 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    route_match = match(req)
+
+    if route_match.nil?
+      res.status = 404
+    else
+      route_match.run(req, res)
+    end
   end
 end
--- W5D2/skeleton/lib/session.rb
+++ W5D2/skeleton/lib/session.rb
@@ -4,16 +4,26 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookie = req.cookies["_rails_lite_app"]
+    if cookie
+      @content = JSON.parse(cookie)
+    else
+      @content = {}
+    end
   end
 
   def [](key)
+    @content[key]
   end
 
   def []=(key, val)
+    @content[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    cookie = { path: '/', value: @content.to_json }
+    res.set_cookie('_rails_lite_app', cookie)
   end
 end
