90c39fc296eaac4a249a4e089ca547638373a4e6
Spencer Chan <schan1031@gmail.com>
2017-07-02 19:35:13 -0700

    javascript practice

--- Practice/6-1jsprac.js
+++ Practice/6-1jsprac.js
@@ -1,3 +1,19 @@
-Function.prototype.myBind (context) {
+Function.prototype.myBind = function(context) {
   return () => this.apply(context);
 }
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const lamp = new Lamp();
+
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+myBoundTurnOn();
